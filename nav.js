d7d058604afcf3650cd44c1b779a9d72f9b71655
nazu4c <126297215+nazu4c@users.noreply.github.com>
2025-09-08 13:22:23 +0200

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,15 +1,3 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () =>{
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () =>{
-    container.classList.remove("active");
-});
-
 window.addEventListener('load', () => {
   const loadingScreen = document.getElementById('loading-screen');
   const loadingContent = document.querySelector('.loading-content');
